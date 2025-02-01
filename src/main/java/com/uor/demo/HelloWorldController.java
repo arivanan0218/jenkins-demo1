e4ec2a0f98973471880bb654f71cf965bbd66526
arivanan0218 <arivanan_v_e23@engug.ruh.ac.lk>
2025-02-01 16:00:01 +0530

    Forcefully give the error for testing

--- src/main/java/com/uor/demo/HelloWorldController.java
+++ src/main/java/com/uor/demo/HelloWorldController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
 
     @GetMapping("/hello")
-    public String sayHello() {
+    public sayHello() {
         return "Hello, World!";
     }
 
