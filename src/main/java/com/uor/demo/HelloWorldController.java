9ceaad467bc7c7766464d746669eaccc9bb6e27c
arivanan0218 <arivanan_v_e23@engug.ruh.ac.lk>
2025-02-01 16:17:10 +0530

    Correct the error

--- src/main/java/com/uor/demo/HelloWorldController.java
+++ src/main/java/com/uor/demo/HelloWorldController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
 
     @GetMapping("/hello")
-    public sayHello() {
+    public String sayHello() {
         return "Hello, World!";
     }
 
